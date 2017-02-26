c8f92ac1b2f9b5c1dce17ed41a8eb6fe6242cb72
Matteo Merzi <matteo.merzi@gmail.com>
2017-02-26 18:14:18 +0100

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,12 @@
 var socket = io();
 socket.on('connect', function() {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        to: 'Kristen',
-        text: 'Babyyyy <3!'
-    });
 });
 
 socket.on('disconnect', function() {
     console.log('Disconnected from server');
 });
 
-socket.on('newMessage', function(newMessage) {
-    console.log('New message', newMessage);
+socket.on('newMessage', function(message) {
+    console.log('newMessage', message);
 });
